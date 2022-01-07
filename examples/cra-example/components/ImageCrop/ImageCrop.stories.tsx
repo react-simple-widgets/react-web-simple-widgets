03b9b34d2246f267fc4c89b0175915dadf098636
Hoàng Ng <hoang.nguyen1247@gmail.com>
2022-01-07 07:54:53 +0700

    fix: update zoom

--- examples/cra-example/components/ImageCrop/ImageCrop.stories.tsx
+++ examples/cra-example/components/ImageCrop/ImageCrop.stories.tsx
@@ -5,6 +5,7 @@ import getCroppedImg from "./cropImage";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Slider from "@mui/material/Slider";
+import Box from "@mui/material/Box";
 
 const dogImg = 'https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000';
 
@@ -12,7 +13,7 @@ const DefaultExample = (props) => {
 
     const [crop, setCrop] = React.useState({ x: 0, y: 0 })
     const [rotation, setRotation] = React.useState(0)
-    const [zoom, setZoom] = React.useState(1)
+    const [zoom, setZoom] = React.useState(0.5)
     const [croppedAreaPixels, setCroppedAreaPixels] = React.useState(null)
     const [croppedImage, setCroppedImage] = React.useState(null)
 
@@ -40,8 +41,14 @@ const DefaultExample = (props) => {
 
     return (
         <div>
-            <div
+            <Box
                 // className={classes.cropContainer}
+                sx={{
+                    position: "relative",
+                    width: "300px",
+                    height: "300px",
+                    background: "#333",
+                }}
             >
                 {/* @ts-ignore */}
                 <Cropper
@@ -49,38 +56,64 @@ const DefaultExample = (props) => {
                     crop={crop}
                     rotation={rotation}
                     zoom={zoom}
+                    minZoom={0.5}
                     aspect={4 / 3}
                     onCropChange={setCrop}
                     onRotationChange={setRotation}
                     onCropComplete={onCropComplete}
                     onZoomChange={setZoom}
+                    objectFit="contain"
+                    restrictPosition={false}
                 />
-            </div>
-            <div
+            </Box>
+            <Box
                 // className={classes.controls}
+                sx={{
+                    padding: "16px",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                }}
             >
-                <div
+                <Box
                     // className={classes.sliderContainer}
+                    sx={{
+                        display: "flex",
+                        flex: "1",
+                        alignItems: "center",
+                    }}
                 >
                     <Typography
                         variant="overline"
                         // classes={{ root: classes.sliderLabel }}
+                        sx={{
+                        }}
                     >
                         Zoom
                     </Typography>
                     <Slider
                         value={zoom}
-                        min={1}
+                        min={0.5}
                         max={3}
                         step={0.1}
                         aria-labelledby="Zoom"
                         // classes={{ root: classes.slider }}
+                        sx={{
+                            padding: "22px 0px",
+                            marginLeft: "32px",
+                        }}
                         // @ts-ignore
                         onChange={(e, zoom) => setZoom(zoom)}
                     />
-                </div>
-                <div
+                </Box>
+                <Box
                     // className={classes.sliderContainer}
+                    sx={{
+                        display: "flex",
+                        flex: "1",
+                        alignItems: "center",
+                        marginLeft: "16px",
+                    }}
                 >
                     <Typography
                         variant="overline"
@@ -95,20 +128,36 @@ const DefaultExample = (props) => {
                         step={1}
                         aria-labelledby="Rotation"
                         // classes={{ root: classes.slider }}
+                        sx={{
+                            padding: "22px 0px",
+                            marginLeft: "32px",
+                        }}
                         // @ts-ignore
                         onChange={(e, rotation) => setRotation(rotation)}
                     />
-                </div>
+                </Box>
                 <Button
                     onClick={showCroppedImage}
                     variant="contained"
                     color="primary"
                     // classes={{ root: classes.cropButton }}
+                    sx={{
+                        marginLeft: "16px",
+                    }}
                 >
                     Show Result
                 </Button>
-            </div>
+            </Box>
             {/* <ImgDialog img={croppedImage} onClose={onClose} /> */}
+            {croppedImage &&
+                <img
+                    src={croppedImage}
+                    style={{
+                        width: "300px",
+                        height: "auto",
+                    }}
+                />
+            }
         </div>
     )
 };
